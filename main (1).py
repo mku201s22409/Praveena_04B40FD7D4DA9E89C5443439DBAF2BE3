c2ebd94a94ec0b659f35a3cf6dfdee85c4982d49
mku201s22409 <144368621+mku201s22409@users.noreply.github.com>
2023-09-07 22:24:23 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,19 @@
+# Write a program that determines whether a year entered by the user is a leap year or not using ifelif-else statements
+
+
+
+
+
+
+def leap(year):
+  if(year %4==0 and year%100!=0 or year %400==0):
+    return True
+  else: 
+    return False
+
+year=int(input("Enter the year :"))
+
+if leap(year):
+    print("is a leap year:",year)
+else:
+   print("is a not leap year:", year)
